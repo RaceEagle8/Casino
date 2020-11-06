6c7122c700ca53be4f36e3d2eab927bc837dfdef
quoxiii <lordofbedwars@gmail.com>
2020-11-06 17:03:19 +0100

    Update Roulette

--- Casino/src/main/java/com/mycompany/casino/RouletteViewController.java
+++ Casino/src/main/java/com/mycompany/casino/RouletteViewController.java
@@ -81,117 +81,119 @@ public class RouletteViewController implements Initializable {
        tfRandomNumber.setText(String.valueOf(rouletteNumber)); // int Wert wird als String in das Textfeld geschrieben
        
        if(rouletteNumber == 00){
-           
+           RouletteRad.setRotate(180);
        }
        else if(rouletteNumber == 0){
-           
+           RouletteRad.setRotate(180);
        }
        else if(rouletteNumber == 1){
            RouletteRad.setRotate(10);
        }
        else if(rouletteNumber == 2){
-           RouletteRad.setRotate(-120);
+           RouletteRad.setRotate(-170);
        }
        else if(rouletteNumber == 3){
-           RouletteRad.setRotate(35);
+           RouletteRad.setRotate(48);
        }
        else if(rouletteNumber == 4){
-           RouletteRad.setRotate(-75);
+           RouletteRad.setRotate(-132);
        }
        else if(rouletteNumber == 5){
            RouletteRad.setRotate(85);
        }
        else if(rouletteNumber == 6){
-           RouletteRad.setRotate(-50);
+           RouletteRad.setRotate(-94);
        }
        else if(rouletteNumber == 7){
-           RouletteRad.setRotate(75);
+           RouletteRad.setRotate(124);
        }
        else if(rouletteNumber == 8){
-           RouletteRad.setRotate(-35);
+           RouletteRad.setRotate(-57);
        }
        else if(rouletteNumber == 9){
-           
+           RouletteRad.setRotate(162);
        }
        else if(rouletteNumber == 10){
-           
+           RouletteRad.setRotate(-18);
        }
        else if(rouletteNumber == 11){
-           
+           RouletteRad.setRotate(133);
        }
        else if(rouletteNumber == 12){
-           
+           RouletteRad.setRotate(-46);
        }
        else if(rouletteNumber == 13){
-           
+           RouletteRad.setRotate(20);
        }
        else if(rouletteNumber == 14){
-           
+           RouletteRad.setRotate(-161);
        }
        else if(rouletteNumber == 15){
-           
+           RouletteRad.setRotate(57);
        }
        else if(rouletteNumber == 16){
-           
+           RouletteRad.setRotate(-123);
        }
        else if(rouletteNumber == 17){
-           
+           RouletteRad.setRotate(95);
        }
        else if(rouletteNumber == 18){
-           
+           RouletteRad.setRotate(-85);
        }
        else if(rouletteNumber == 19){
-           
+           RouletteRad.setRotate(-66);
        }
        else if(rouletteNumber == 20){
-           
+           RouletteRad.setRotate(114);
        }
        else if(rouletteNumber == 21){
-           
+           RouletteRad.setRotate(-104);
        }
        else if(rouletteNumber == 22){
-           
+           RouletteRad.setRotate(76);
        }
        else if(rouletteNumber == 23){
-           
+           RouletteRad.setRotate(-142);
        }
        else if(rouletteNumber == 24){
-           
+           RouletteRad.setRotate(38); 
        }
        else if(rouletteNumber == 25){
-           
+           RouletteRad.setRotate(-28);
        }
        else if(rouletteNumber == 26){
-           
+           RouletteRad.setRotate(152);
        }
        else if(rouletteNumber == 27){
-           
+           RouletteRad.setRotate(-10);
        }
        else if(rouletteNumber == 28){
-           
+           RouletteRad.setRotate(171);
        }
        else if(rouletteNumber == 29){
-           
+           RouletteRad.setRotate(-38);
        }
        else if(rouletteNumber == 30){
-           
+           RouletteRad.setRotate(143);
        }
        else if(rouletteNumber == 31){
-           
+           RouletteRad.setRotate(-75);
        }
        else if(rouletteNumber == 32){
-           
+           RouletteRad.setRotate(104);
        }
        else if(rouletteNumber == 33){
-           
+           RouletteRad.setRotate(-113);
        }
        else if(rouletteNumber == 34){
-           
+           RouletteRad.setRotate(67);
        }
        else if(rouletteNumber == 35){
-           
+           RouletteRad.setRotate(-151);
        }
-       
+       else if(rouletteNumber == 36){
+           RouletteRad.setRotate(30);
+       }
        
        
     }
